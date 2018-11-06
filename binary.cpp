5726a79bd5c571f85ca6f3bfc03be7fac98421fe
combermere246 <combermere246@users.noreply.github.com>
2018-11-06 13:22:56 -0400

    text.cpp

---
+++ binary.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+   
+   ofstream out{"abc.bin", ios::binary};
+   if (out)
+   {
+	   out.write (reinterpret_cast<char*>(&a),sizeof(a));
+	   out.write (reinterpret_cast<char*>(&b),sizeof(b));
+	   out.write (reinterpret_cast<char*>(&c),sizeof(c));
+   }
+   else
+   {
+	   cout << "Error writing to file abc.bin" << endl;
+   }
+ 
+   return 0;
+}
---
+++ numbers.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   char a;
+   int b;
+   float c; 
+   float d;
+   int numbers = 0;
+   int count = 0;
+   int average;
+   
+   
+	   
+   ifstream in{"bonus"};
+   
+   if (in)
+   {
+	   for (int i=0; i<31; i++)
+		{
+	   in >> a;
+	   in >> b;
+	   in >> c;
+	   in >> d;
+	   
+			if (a == 'G')
+			{
+	   
+	   numbers += b;
+	   
+	   count+=1;
+	   
+	   cout << a << " " << b << " " << c << " " << d << endl;
+	   
+	   
+			}
+		
+		}
+		average = numbers/count;
+		cout << endl << "The average of all integers are " << average << endl<< endl;
+	}
+   else
+   {
+	   cout << "Error reading from file bonus" << endl;
+   
+}
+   
+   
+ 
+   return 0;
+}
